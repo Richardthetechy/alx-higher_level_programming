35d57c014be9d36ea2e2fad9f5b22464699d2074
Richardthetechy <techchuks16@gmail.com>
2025-03-18 18:26:25 +0100

    checker task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,11 +7,12 @@ class Square:
                 size (int)
         """
         self.size = size
-    
+
     @property
     def size(self):
         """Return __size"""
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -19,7 +20,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Compute area of squar"""
         return self.__size ** 2
